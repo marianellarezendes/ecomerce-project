8bd45c0109b0be2f9dc060a49bd61e0c7c55eef9
marianellarezendes <69059570+marianellarezendes@users.noreply.github.com>
2020-08-16 21:48:42 -0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,6 +1,8 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-
-});
+$("#btnLogin").on("click",function(){
+    let user = $("#emailInput").val();
+    let pass = $("#passInput").val();
+    
+    if(user!="" && pass!=""){
+        location.href="inicio.html"
+    }
+})
